786c3ead7fa094e710fc53a32c1557d1ec2d602b
AWhiteRedAcademy <Aidan.White@redacademy.co.za>
2026-04-30 14:33:01 +0200

    Fixed VehicleModel from Prior .Json conversion tests

--- VehicleBookApiApp/VehicleBookApi/Models/VehicleModel.cs
+++ VehicleBookApiApp/VehicleBookApi/Models/VehicleModel.cs
@@ -2,18 +2,20 @@
 {
     public class VehicleModel
     { 
-           /* public int VehicleID { get; set; }
+            public int VehicleID { get; set; }
             public int OwnerID { get; set; }
             public required string Make { get; set; }
             public required string Model { get; set; }
             public required string Category { get; set; }
             public float DailyRate { get; set; }
             public bool IsAvailable { get; set; }
-           */
+           
+           /*
             public int ProductID { get; set; }
             public string Name { get; set; } = string.Empty;
             public string Description { get; set; } = string.Empty;
             public decimal Price { get; set; }
             public int Stock { get; set; }
+           */
     }
 }
--- VehicleBookApiApp/VehicleBookApi/obj/Debug/net10.0/VehicleBookApi.AssemblyInfo.cs
+++ VehicleBookApiApp/VehicleBookApi/obj/Debug/net10.0/VehicleBookApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VehicleBookApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d712048e5f5c980253d7ce14142b92b8175543d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("VehicleBookApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VehicleBookApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
