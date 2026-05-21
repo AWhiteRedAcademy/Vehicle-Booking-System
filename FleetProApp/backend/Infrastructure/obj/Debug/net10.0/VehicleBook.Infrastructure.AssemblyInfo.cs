253267d6193ff9fe34ea711d0c0501a0faa272b0
ubayd-dev <thaqibubayd@gmail.com>
2026-05-21 15:04:58 +0200

    Added Routes App.jsx

--- FleetProApp/backend/Infrastructure/obj/Debug/net10.0/VehicleBook.Infrastructure.AssemblyInfo.cs
+++ FleetProApp/backend/Infrastructure/obj/Debug/net10.0/VehicleBook.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VehicleBook.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcdc00fa0910bc486cdf3a3e231dbf7222332120")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0c002aa228a3d871019a5e622b0e0fd0ae89a20")]
 [assembly: System.Reflection.AssemblyProductAttribute("VehicleBook.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VehicleBook.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- FleetProApp/frontend/src/App.jsx
+++ FleetProApp/frontend/src/App.jsx
@@ -1,11 +1,31 @@
 // import { useState } from 'react'
-import LoginPage from './pages/LoginPage/LoginPage'
+import LoginPage from "./pages/LoginPage/LoginPage";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import LoginPage from "./pages/LoginPage/LoginPage";
+// import DashboardPage from "./pages/DashboardPage/DashboardPage";
+// import RegisterPage from "./pages/RegisterPage/RegisterPage";
 
+function DashboardPage() {
+  return <h1>Dashboard Page</h1>;
+}
+
+function RegisterPage() {
+  return <h1>Register Page</h1>;
+}
 
 function App() {
   return (
-   <LoginPage/>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+  ƒ;
 }
 
-export default App
+export default App;
